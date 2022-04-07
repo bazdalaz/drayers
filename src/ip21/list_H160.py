34c9e281bbe68cbc93101b2aa9f26b84035066ac
Bazdalaz <bazdalaz@users.noreply.github.com>
2022-04-07 14:43:54 +0300

    updated listst

--- src/ip21/list_H160.py
+++ src/ip21/list_H160.py
@@ -1,10 +1,10 @@
 TAGS = [
+    "73_H160.NN(59)",
     "73_H160.PHASE",
-    "73_H160.STEP",
-    "73_H160.STATMENT",
     "73_H160.NN(33)",
     "GWI4801.PV",
     "GTI8031.PV",
     "GPI8030.PV"
+    "GTI3708.PV",
 ]
 unit_name = 'H160'
--- src/ip21/list_H161.py
+++ src/ip21/list_H161.py
@@ -1,11 +1,11 @@
 TAGS = [
+    "73_H161.NN(59)",
     "73_H161.PHASE",
-    "73_H161.STEP",
-    "73_H161.STATMENT",
     "73_H161.NN(33)",
     "GWI4802.PV",
     "GTI4759.PV",
-    "GPI4452.PV"
+    "GPI4452.PV",
+    "GTI4789.PV",
 ]
 
 unit_name = 'H161'
